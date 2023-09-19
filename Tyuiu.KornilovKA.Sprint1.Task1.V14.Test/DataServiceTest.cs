3d0a2e5a32df1962d209ff5b6e7948041a83b786
kilianAid <kilian_bs@mail.ru>
2023-09-19 20:31:39 +0500

    Task1

---
+++ Tyuiu.KornilovKA.Sprint1.Task1.V14.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KornilovKA.Sprint1.Task1.V14.Lib;
+
+namespace Tyuiu.KornilovKA.Sprint1.Task1.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double a = 2.0;
+            double b = 2.0;
+            double c = 2.0;
+
+            var res = ds.Calculate(a, b, c);
+            Assert.AreEqual(2.5, res);
+        }
+    }
+}
